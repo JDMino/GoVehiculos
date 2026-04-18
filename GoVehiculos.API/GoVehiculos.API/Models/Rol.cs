516a858ecc1e684dd3ca54c191ff038d7ddadcc3
JDMino <jdminogomez@gmail.com>
2026-04-18 11:03:14 -0300

    fix: backend-login

--- GoVehiculos.API/GoVehiculos.API/Models/Rol.cs
+++ GoVehiculos.API/GoVehiculos.API/Models/Rol.cs
@@ -1,11 +1,16 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema; 
 
 namespace GoVehiculos.API.Models
 {
+    [Table("Rol")] 
     public class Rol
     {
         [Key]
+        [Column("id_rol")] 
         public int IdRol { get; set; }
+
+        [Column("nombre")]
         public string Nombre { get; set; } = string.Empty;
     }
 }
